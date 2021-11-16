dcf73b7413d2b9144e951f46b3bc4fba40e4f49d
Vlas_Mastykash <mastykash.vlas@gmail.com>
2021-11-16 21:27:47 +0600

    del script

--- concat.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /f/courses/github_repos/pdf-presentations/
-git pull
-cd /f/courses/github_repos/v-las/
-git pull
-cat /f/courses/github_repos/v-las/title.md /f/courses/github_repos/pdf-presentations/README.md /f/courses/github_repos/v-las/ending.md > README.md
-git commit -am 'Update README.md'
-git push
